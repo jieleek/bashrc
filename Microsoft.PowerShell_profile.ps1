8860eb320f22cb3fe6064ecfe20eccbc7648a86c
Jie Li <jieleek@gmail.com>
2023-03-15 15:09:08 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,40 +1,42 @@
 # ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Set-PSReadlineOption -PredictionSource History
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Key Escape,Ctrl+p -Function HistorySearchBackward
-Set-PSReadLineKeyHandler -Chord Escape,p -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadlineOption -BellStyle None
-# vpnkit
-# wsl.exe -l --running
-# wsl.exe -d wsl-vpnkit service wsl-vpnkit start
-# After install z.lua
-Invoke-Expression (& { (lua $HOME\scoop\apps\z.lua\current\z.lua --init powershell enhanced once) -join "`n" })
-Remove-Item Alias:curl
+Invoke-Expression (& { (lua C:\Users\jie.li\scoop\apps\z.lua\current\z.lua --init powershell enhanced once) -join "`n" })
+# Remove-Item Alias:curl
 Remove-Item Alias:ls
 Remove-Item Alias:cp
 Remove-Item Alias:gl -Force
 Remove-Item Alias:diff -Force
 Remove-Item Alias:sort -Force
 Remove-Item Alias:tee -Force
-Remove-Item Alias:wget
+# Remove-Item Alias:wget
 Remove-Item Alias:cat
 Remove-Item Alias:mv
 Remove-Item Alias:rm
 Remove-Item Alias:echo
-Remove-Item Alias:foreach -Force
-Remove-Item Alias:man
-Remove-Item Alias:mount
-Remove-Item Alias:ps
-Remove-Item Alias:pushd
-Remove-Item Alias:pwd
-Remove-Item Alias:set
-Remove-Item Alias:start -Force
-Remove-Item Alias:where -Force
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+If (Test-Path "C:\Users\jie.li\scoop\apps\miniconda3\current\Scripts\conda.exe") {
+    (& "C:\Users\jie.li\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+#endregion
+
+function Invoke-LsColor {
+    & 'ls.exe' --color $args
+}
+
+New-Alias -Name ls -Value Invoke-LsColor
+
 function gl() {
 	& git log --oneline --all --graph --decorate
 }
-
 function Write-BranchName ($p1, $p2, $conda) {
+
+
     try {
         $branch = git rev-parse --abbrev-ref HEAD
 
@@ -85,10 +87,5 @@ function prompt {
 
     return $userPrompt
 }
-
 function prompt2 { "PS $(Get-Location)`r`n$ " }
-# git prompt
-# Import-Module posh-git
-# Add-PoshGitToProfile -AllHost
-# oh-my-posh init pwsh | Invoke-Expression
-# oh-my-posh init pwsh --config 'aliens\jandedobbeleer.omp.json' | Invoke-Expression
+
